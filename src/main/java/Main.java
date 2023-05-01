217471daf12839f7cbbad3649876db24d309579d
bispoalef <alef.b.dos.santos.1993@gmail.com>
2023-04-30 21:40:58 -0300

    Criando classe para posição das peças

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
-package org.example;
+import tabuleiro.Posicao;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Posicao ps = new Posicao(1,1);
+
+
+        System.out.println(ps.toString());
+
     }
 }
